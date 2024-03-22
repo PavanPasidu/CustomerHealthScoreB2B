155c2c07f7e40efdbc074c48fe5848cc4df8aff2
Pavan Pasidu <116521529+PavanPasidu@users.noreply.github.com>
2024-03-22 10:31:12 +0530

    Add files via upload

---
+++ Inference/inference.py
@@ -0,0 +1,8 @@
+'''
+Here I am using trained model and predict the healthscore.And 
+also I will get sentiment value through an API and combine it
+with healthscore. Combining sentiment to healthscore will be 
+done dailybasis.
+
+'''
+
